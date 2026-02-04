a3006ab566655e85be9e436c4bd4a76f1dd73a98
Laetitia <laeptitchat29@gmail.com>
2026-02-04 16:55:36 +0100

    update .env back

--- back/main.py
+++ back/main.py
@@ -1,21 +1,25 @@
+from dotenv import load_dotenv
+from pathlib import Path
 import os
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 
+ENV_PATH = Path(__file__).resolve().parent 
+load_dotenv(dotenv_path=ENV_PATH)
 app = FastAPI()
 
-origins_env = os.getenv(
-    "FRONTEND_ORIGINS",
-    "http://localhost:3000",
-)
+print("ENV FILE:", ENV_PATH)
+print("EXISTS:", ENV_PATH.exists())
+print("FRONTEND_ORIGINS:", repr(os.getenv("FRONTEND_ORIGINS")))
+origins_env = os.getenv("FRONTEND_ORIGINS", "")
 
 allowed_origins = [o.strip() for o in origins_env.split(",") if o.strip()]
 
 app.add_middleware(
     CORSMiddleware,
     allow_origins=allowed_origins,
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
