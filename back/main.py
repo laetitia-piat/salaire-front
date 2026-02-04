986281a471fa76c6e794cdc8048a7460c89b6f62
Laetitia <laeptitchat29@gmail.com>
2026-02-04 18:15:06 +0100

    fix import env

--- back/main.py
+++ back/main.py
@@ -1,19 +1,16 @@
-from dotenv import load_dotenv
 from pathlib import Path
+from dotenv import load_dotenv
 import os
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 
-ENV_PATH = Path(__file__).resolve().parent 
+ENV_PATH = Path(__file__).resolve().parent / ".env"
 load_dotenv(dotenv_path=ENV_PATH)
+
 app = FastAPI()
 
-print("ENV FILE:", ENV_PATH)
-print("EXISTS:", ENV_PATH.exists())
-print("FRONTEND_ORIGINS:", repr(os.getenv("FRONTEND_ORIGINS")))
 origins_env = os.getenv("FRONTEND_ORIGINS", "")
-
 allowed_origins = [o.strip() for o in origins_env.split(",") if o.strip()]
 
 app.add_middleware(
